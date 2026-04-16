1dbe46da39128169ea0ed719508d552a7b0954d4
mdasikuzzaman <mdasikuzzaman.en@gmail.com>
2026-04-16 22:05:31 +0600

    refactor: uncomment proxy authentication logic and configuration

--- proxy.ts
+++ proxy.ts
@@ -3,21 +3,21 @@ import type { NextRequest } from "next/server";
 import { auth } from "./lib/auth";
 
 export async function proxy(req: NextRequest) {
-  // const token = await auth();
+  const token = await auth();
 
-  // const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+  const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
 
-  // if (isAdminRoute) {
-  //   if (!token || token.user?.role !== "admin") {
-  //     return NextResponse.redirect(
-  //       new URL("/api/auth/signin?callbackUrl=/admin", req.url),
-  //     );
-  //   }
-  // }
+  if (isAdminRoute) {
+    if (!token || token.user?.role !== "admin") {
+      return NextResponse.redirect(
+        new URL("/api/auth/signin?callbackUrl=/admin", req.url),
+      );
+    }
+  }
 
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: ["/admin/:path*"],
-// };
+export const config = {
+  matcher: ["/admin/:path*"],
+};
