e09d78585efa2562c9be29457eec01d556635e3b
mdasikuzzaman <mdasikuzzaman.en@gmail.com>
2026-04-16 21:50:06 +0600

    fix: resolve merge conflicts in Prisma schema and update global Prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,8 @@
 // lib/prisma.ts
 import { PrismaClient } from "../lib/generated/prisma";
 
-<<<<<<< HEAD
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-=======
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
->>>>>>> 2d0dfafbfea5162e70e8659b24a736ed2a955acd
 };
 
 export const prisma =
